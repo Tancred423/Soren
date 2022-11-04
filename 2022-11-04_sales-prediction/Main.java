aab07f1d59cd0dbad9a3fe259f944ec5e047dfd3
Tancred423 <thonkred@gmail.com>
2022-11-04 21:26:49 +0100

    Add sales prediction task

---
+++ 2022-11-04_sales-prediction/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static int profitPercentage = 23;
+
+    public static void main(String[] args) {
+        System.out.println("Please provide the projected amount of total sales: ");
+
+        int totalSales = getInputAsInteger();
+        int annualProfit = getAnnualProfit(totalSales);
+
+        System.out.println("The annual profit is: " + annualProfit);
+
+        scanner.close();
+    }
+
+    private static int getInputAsInteger() {
+
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        } catch (Exception e) {
+            System.out.println("[!] This is not a valid number. Try again: ");
+            return getInputAsInteger();
+        }
+    }
+
+    private static int getAnnualProfit(int totalSales) {
+        return Math.round(totalSales * profitPercentage / 100);
+    }
+}
---
+++ 2022-11-04_sales-prediction/README.md
@@ -0,0 +1,11 @@
+# Sales Prediction
+
+## Task
+
+A company has determined that its annual profit is typically 23 percent of total sales. Design a program that asks the user to enter the projected amount of total sales, and then display the profit that will be made from that amount.
+
+## Restrictions
+
+REMEMBER WE ARE USING FUNCTIONS
+
+## Result
